84493cd13a652f7be385f1050f33f06abded82b7
Simarpreet <simarpreet811@gmail.com>
2020-07-04 19:35:03 +0530

    Dijkstra with Direction Penalty

--- android-expo/utility/Dijkstra.js
+++ android-expo/utility/Dijkstra.js
@@ -6,51 +6,63 @@ const floor16 = JSON.parse(floorData16);
 const floorData17 = fs.readFileSync('../floor_plans/floor_17/floor.json');
 const floor17 = JSON.parse(floorData17);
 
+const penalty = 1;
+const PI = 3.1415926;
+
 const floors = {
 	'16': floor16,
 	'17': floor17
 };
 
+class PriorityQueue {
+	constructor() {
+		this.collection = [];
+  	}
+
+	enqueue(element) {
+    	if (this.isEmpty()){ 
+    		this.collection.push(element);
+    	} else {
+	    	let added = false;
+	      	for (let i = 1; i <= this.collection.length; i++) {
+	        	if (element[1] < this.collection[i-1][1]) { 
+	         		this.collection.splice(i-1, 0, element);
+	          		added = true;
+	          		break;
+	        	}
+	      	}
+	      	if (!added) {
+	        	this.collection.push(element);
+	      	}
+    	}
+  	};
+
+  	dequeue() {
+    	let value = this.collection.shift();
+    	return value;
+  	};  
+
+  	isEmpty() {
+    	return (this.collection.length === 0) 
+  	};
+}
+
 const get_distance = (p1, p2) => {
 	return Math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2);
 };
 
-class PriorityQueue {
-  constructor() {
-    this.collection = [];
-  }
-
-  enqueue(element) {
-    if (this.isEmpty()){ 
-      this.collection.push(element);
-    } else {
-      let added = false;
-      for (let i = 1; i <= this.collection.length; i++){
-        if (element[1] < this.collection[i-1][1]){ 
-          this.collection.splice(i-1, 0, element);
-          added = true;
-          break;
-        }
-      }
-      if (!added){
-          this.collection.push(element);
-      }
-    }
-  };
-
-  dequeue() {
-    let value = this.collection.shift();
-    return value;
-  };  
-
-  isEmpty() {
-    return (this.collection.length === 0) 
-  };
-}
+const getAngle = (p1, p2, p3) => {
+	let p21 = [p2[0] - p1[0], p2[1] - p1[1]];
+	let p23 = [p2[0] - p3[0], p2[1] - p3[1]];
+	let dotProduct = p21[0] * p23[0] + p21[1] * p23[1];
+	let crossProduct = p21[0] * p23[1] - p21[1] * p23[0];
+	let angle = Math.atan2(crossProduct, dotProduct);
+	return Math.floor(angle * 180 / PI + 0.5);
+};
 
 const Dijkstra = (source, destination, floorNumber) => {
 	let distance = {};
-	let backtrace = {};
+	let parent = {};
 	let pq = new PriorityQueue();
 	let visited = {};
 	distance[source] = 0;
@@ -60,6 +72,7 @@ const Dijkstra = (source, destination, floorNumber) => {
 		if (node != source) {
 			distance[node] = Infinity;
 		}
+		parent[node] = -1;
 	});
 
 	pq.enqueue([source, 0]);
@@ -71,11 +84,16 @@ const Dijkstra = (source, destination, floorNumber) => {
 
 		pathNodes[currentNode]["green_adjacent"].forEach(node => {
 			let newDistance = distance[currentNode] + get_distance(pathNodes[currentNode]["coords"], pathNodes[node]["coords"]);
+			if (parent[currentNode] != -1) {
+				let angle = getAngle(pathNodes[parent[currentNode]]["coords"], pathNodes[currentNode]["coords"], pathNodes[node]["coords"]);
+				angle = 180 - Math.abs(angle);
+				newDistance += angle * penalty;
+			}
 
 			if (!visited[node] && newDistance < distance[node]) {
 				distance[node] = newDistance;
-				backtrace[node] = currentNode;
-				pq.enqueue([node, distance]);
+				pq.enqueue([node, distance[node]]);
+				parent[node] = currentNode;
 			}
 		}); 
 	}
@@ -84,8 +102,8 @@ const Dijkstra = (source, destination, floorNumber) => {
 	let lastNode = destination;
 
 	while (lastNode !== source) {
-		path.unshift(backtrace[lastNode]);
-		lastNode = backtrace[lastNode];
+		path.unshift(parent[lastNode]);
+		lastNode = parent[lastNode];
 	}
 	return path;
 };
@@ -128,4 +146,4 @@ const getPath = (source, destination) => {
 	}
 };
 
-console.log(getPath("16.3H.1", "17.3B.1"));
+console.log(getPath("16.3G.2", "16.3C.1"));
