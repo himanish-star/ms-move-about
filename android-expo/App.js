1eb8f424a510dfe8bd7c6f1d00585e73bbf9d183
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-07-10 10:45:56 +0530

    push notifications integrated

--- android-expo/App.js
+++ android-expo/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {AsyncStorage, Image, Platform, Vibration} from 'react-native';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import { Block, GalioProvider } from 'galio-framework';
@@ -7,6 +7,14 @@ import { Block, GalioProvider } from 'galio-framework';
 import Screens from './navigation/Screens';
 import { Images, articles, argonTheme } from './constants';
 import * as Font from "expo-font";
+import Constants from 'expo-constants';
+import {Notifications} from 'expo';
+import * as Permissions from 'expo-permissions';
+import firebaseConfig from "./firebaseConfig.json";
+import * as firebase from 'firebase';
+import {EventRegister} from "react-native-event-listeners";
+
+firebase.initializeApp(firebaseConfig);
 
 // cache app images
 const assetImages = [
@@ -18,6 +26,9 @@ const assetImages = [
   Images.iOSLogo,
   Images.androidLogo
 ];
+
+// TODO: make actual setup of uid and login :)
+const currentUser = "soumyam";
 
 // cache product images
 articles.map(article => assetImages.push(article.image));
@@ -36,14 +47,60 @@ export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
     fontLoaded: false,
+    expoPushToken: null,
+    notification: {},
   }
 
   async componentDidMount() {
+    this.registerForPushNotificationsAsync();
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
     await Font.loadAsync({
       'roboto-medium': require('./screens/fonts/Roboto/Roboto-Medium.ttf')
     });
     this.setState({fontLoaded: true});
   }
+
+  _handleNotification = notification => {
+    Vibration.vibrate();
+    console.log(notification);
+    if(notification.origin==="selected") {
+      EventRegister.emit("push-notification-data", notification.data);
+      AsyncStorage.setItem("push-notification-data", JSON.stringify(notification.data));
+    }
+    this.setState({ notification: notification });
+  };
+
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Push Notifications Disabled');
+        return;
+      }
+      const token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+      this.setState({ expoPushToken: token });
+      let updates = {};
+      updates['/expoToken'] = token
+      firebase.database().ref('users').child(currentUser).update(updates);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('default', {
+        name: 'default',
+        sound: true,
+        priority: 'max',
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  };
 
   render() {
     if(!this.state.isLoadingComplete || !this.state.fontLoaded) {
