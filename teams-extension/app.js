74da06335391794ff3e2316aa168ebaf91ce8659
Simarpreet <simarpreet811@gmail.com>
2020-07-10 18:05:40 +0530

    Push notif automated using MutateObserver

--- teams-extension/app.js
+++ teams-extension/app.js
@@ -1,46 +1,58 @@
+const sendNotification = async (messages) => {
+	for (let i=messages.length-1 ; i>=0 ; i--) {
+		let msg_i = messages[i].trim();
+		if (msg_i.startsWith('/route')) {
+			const roomId = msg_i.substr(6).trim();
+			const message = {
+			    to: 'ExponentPushToken[IUbUT6PZOruE8cKuooKPBc]',
+			    sound: 'default',
+			    title: 'Destination ID',
+			    body: 'Destination ID for MS Move About App',
+			    data: { dest: roomId },
+			};
+			const proxyurl = "https://cors-anywhere.herokuapp.com/";
+			const url = 'https://exp.host/--/api/v2/push/send';
+			return await fetch(proxyurl + url, {
+			    method: 'POST',
+			    headers: {
+			      	Accept: 'application/json',
+			      	'Accept-encoding': 'gzip, deflate',
+			      	'Content-Type': 'application/json',
+			      	'Access-Control-Allow-Origin': '*'
+			    },
+			    body: JSON.stringify(message),
+			})
+			break;
+		}
+	}
+};
+
 chrome.tabs.getSelected(null, tab => {
 	if (tab.url.includes('teams.microsoft.com')) {
 		document.querySelector('.disclaimer').classList.add('hidden');
-
 		document.querySelector('.route').addEventListener('click', () => {
 			chrome.tabs.sendMessage(tab.id, {
 				action: 'fetchMessages'
 			}, response => {
-				for (let i=response.length-1 ; i>=0 ; i--) {
-					let msg_i = response[i].trim();
-					if (msg_i.startsWith('/route')) {
-						const roomId = msg_i.substr(6).trim();
-						const message = {
-						    to: 'EXPO_ID',
-						    sound: 'default',
-						    title: 'Destination ID',
-						    body: 'Destination ID for MS Move About App',
-						    data: { dest: roomId },
-						};
-						const proxyurl = "https://cors-anywhere.herokuapp.com/";
-						const url = 'https://exp.host/--/api/v2/push/send';
-						fetch(proxyurl + url, {
-						    method: 'POST',
-						    headers: {
-						      	Accept: 'application/json',
-						      	'Accept-encoding': 'gzip, deflate',
-						      	'Content-Type': 'application/json',
-						      	'Access-Control-Allow-Origin': '*'
-						    },
-						    body: JSON.stringify(message),
-						})
-						.then((response) => {
-							console.log(response);
-							console.log('Push Notification Sent!');
-						})
-						.catch(err => {
-							console.log('Error');
-							console.log(err);
-						});
-						break;
-					}
-				}
+				sendNotification(response)
+				.then(() => {
+					console.log('Notification Sent!');
+				})
 			});
 		});
 	}
 });
+
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+	if (request.action === 'receiveMessages') {
+		sendNotification(request.messages)
+		.then(() => {
+			console.log('Notification sent!');
+			callback();
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+});
+
--- teams-extension/contentScript.js
+++ teams-extension/contentScript.js
@@ -1,6 +1,28 @@
+const getMessages = () => {
+	return Array.from(document.querySelectorAll('.message-body-content')).map(msg => msg.innerText);
+};
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.action === 'fetchMessages') {
-		const messages = Array.from(document.querySelectorAll('.message-body-content')).map(msg => msg.innerText);
-		sendResponse(messages);
+		sendResponse(getMessages());
 	}
 });
+
+window.onload = () => {
+	setTimeout(() => {
+		const targetNode = document.querySelector('.ts-message-list-container');
+		const config = { childList: true };
+		const callback = (mutationsList, observer) => {
+		    setTimeout(() => {
+		    	chrome.runtime.sendMessage({
+			    	action: 'receiveMessages',
+			    	messages: getMessages()
+			    }, () => {
+			    	console.log('Message Sent!');
+			    });
+		    }, 2000);
+		};
+		const observer = new MutationObserver(callback);
+		observer.observe(targetNode, config);
+	}, 5000);
+}
